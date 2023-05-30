a528cda3af80b410ace9a9a41cea365b821139db
jinoc <choiae369@gmail.com>
2023-05-30 16:43:41 +0900

    코드 정리

--- static/app.js
+++ static/app.js
@@ -173,11 +173,7 @@ window.onload = function() {
     const movies = await fetchMovie();
     const clickMovie = movies.find( item => item.id === parseInt( num ) );
 
-    const title = clickMovie.title;
-    const average = clickMovie.vote_average;
-    const image = clickMovie.poster_path;
-    const overview = clickMovie.overview;
-    const language = clickMovie.original_language;
+    const { title, vote_average, poster_path, overview, original_language } = clickMovie;
 
     const modal = document.querySelector( '.overlay' );
     const modalTitle = document.querySelector( '.modal-body h3' );
@@ -187,9 +183,9 @@ window.onload = function() {
     const modalOverview = document.querySelector( '.modal-content p' );
 
     modalTitle.innerText = title;
-    modalAverage.innerText = average;
-    modalLanguage.innerText = language;
-    modalImg.setAttribute( 'src', `https://image.tmdb.org/t/p/w500/${image}` );
+    modalAverage.innerText = vote_average;
+    modalLanguage.innerText = original_language;
+    modalImg.setAttribute( 'src', `https://image.tmdb.org/t/p/w500/${poster_path}` );
     modalImg.setAttribute( 'alt', title );
     modalOverview.innerText = overview;
 
