f1bab8bd069f3ac84406d7d376a7e4041fe9c63c
jinoc <choiae369@gmail.com>
2023-05-31 15:07:26 +0900

    모듈화 진행 2

--- modules/main.js
+++ modules/main.js
@@ -1,9 +1,10 @@
 import { OPTIONS } from "./options.js";
 import { makeCard } from "./makecard.js";
 import { getMatchMovies } from "./getmatchmovies.js";
+import { setModal } from "./setmodal.js";
 
 //  Fetch
-export const fetchMovie = async function () {
+const fetchMovie = async function () {
   const response = await fetch(
     "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
     OPTIONS
@@ -83,64 +84,43 @@ flexBox.addEventListener("click", (e) => {
   }
 
   const contentId = content.getAttribute("id");
-
   if (contentId === null || contentId === undefined) {
     return false;
   }
 
-  makeModal(contentId);
+  openModal(contentId);
 });
 
 //  Modal make
 const body = document.querySelector("body");
 
-async function makeModal(num) {
+async function openModal(num) {
   const movies = await fetchMovie();
   const clickMovie = movies.find((item) => item.id === parseInt(num));
+  alert(`id : ${num}`);
 
-  const { title, vote_average, poster_path, overview, original_language } =
-    clickMovie;
+  setModal(clickMovie);
 
   const modal = document.querySelector(".overlay");
-  const modalTitle = document.querySelector(".modal-body h3");
-  const modalAverage = document.querySelector(".average");
-  const modalLanguage = document.querySelector(".language");
-  const modalImg = document.querySelector(".modal-content img");
-  const modalOverview = document.querySelector(".modal-content p");
-
-  modalTitle.innerText = title;
-  modalAverage.innerText = vote_average;
-  modalLanguage.innerText = original_language;
-  modalImg.setAttribute(
-    "src",
-    `https://image.tmdb.org/t/p/w500/${poster_path}`
-  );
-  modalImg.setAttribute("alt", title);
-  modalOverview.innerText = overview;
-
   modal.classList.toggle("active");
-  body.classList.toggle("notScroll");
-  alert(`id : ${num}`);
+  body.classList.toggle("notScroll");  
 }
 
 //  focus
 const searchBtn = document.querySelector(".submitBtn");
 const _searchInput = document.getElementById("search-input");
-
 _searchInput.addEventListener("focus", () => {
   _searchInput.classList.toggle("btn-focus");
 });
 _searchInput.addEventListener("blur", () => {
   _searchInput.classList.toggle("btn-focus");
 });
-
 searchBtn.addEventListener("focus", () => {
   searchBtn.classList.toggle("btn-focus");
 });
 searchBtn.addEventListener("blur", () => {
   searchBtn.classList.toggle("btn-focus");
 });
-
 topBtn.addEventListener("focus", () => {
   topBtn.classList.toggle("btn-focus");
 });
@@ -149,9 +129,7 @@ topBtn.addEventListener("blur", () => {
 });
 
 //  Modal close
-
 const overlay = document.querySelector(".overlay");
-
 overlay.addEventListener("click", (e) => {
   if (e.target.className === "overlay") {
     overlay.classList.toggle("active");
@@ -159,9 +137,7 @@ overlay.addEventListener("click", (e) => {
   }
   _searchInput.focus();
 });
-
 const closeBtn = document.querySelector(".close");
-
 closeBtn.addEventListener("click", () => {
   overlay.classList.toggle("active");
   body.classList.toggle("notScroll");
---
+++ modules/setmodal.js
@@ -0,0 +1,20 @@
+export function setModal(clickMovie) {
+  const { title, vote_average, poster_path, overview, original_language } =
+    clickMovie;
+  
+  const modalTitle = document.querySelector(".modal-body h3");
+  const modalAverage = document.querySelector(".average");
+  const modalLanguage = document.querySelector(".language");
+  const modalImg = document.querySelector(".modal-content img");
+  const modalOverview = document.querySelector(".modal-content p");
+
+  modalTitle.innerText = title;
+  modalAverage.innerText = vote_average;
+  modalLanguage.innerText = original_language;
+  modalImg.setAttribute(
+    "src",
+    `https://image.tmdb.org/t/p/w500/${poster_path}`
+  );
+  modalImg.setAttribute("alt", title);
+  modalOverview.innerText = overview;  
+}
