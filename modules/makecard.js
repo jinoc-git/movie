b38b5a18135297ba2dbe7ddbf941e0af96b43c5a
jinoc <choiae369@gmail.com>
2023-06-01 14:11:21 +0900

    카드에 평점 추가

--- modules/makecard.js
+++ modules/makecard.js
@@ -1,10 +1,11 @@
 export function makeCard(movies) {
   for (let i in movies) {
-    const { id, title, poster_path, overview } = movies[i];
+    const { id, title, poster_path, overview, vote_average } = movies[i];
 
     const $box = document.getElementById("flex-box");
     const outerDiv = document.createElement("div");
     const innerDiv = document.createElement("div");
+    const absolDiv = document.createElement("div");
     const img = document.createElement("img");
     const h3 = document.createElement("h3");
     const hr = document.createElement("hr");
@@ -14,7 +15,9 @@ export function makeCard(movies) {
     outerDiv.setAttribute("id", id);
     img.setAttribute("src", `https://image.tmdb.org/t/p/w500/${poster_path}`);
     img.setAttribute("alt", title);
+    absolDiv.setAttribute('class', 'average');
     innerDiv.setAttribute("class", "content-text");
+    absolDiv.innerText = vote_average;
     h3.innerText = title;
     p.innerText = overview;
 
@@ -22,6 +25,7 @@ export function makeCard(movies) {
     innerDiv.appendChild(hr);
     innerDiv.appendChild(p);
     outerDiv.appendChild(img);
+    outerDiv.appendChild(absolDiv);
     outerDiv.appendChild(innerDiv);
     $box.appendChild(outerDiv);
   }
